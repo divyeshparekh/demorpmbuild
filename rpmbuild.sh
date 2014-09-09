661eb0ef388791b309496d73de979a8ee8b81981
Divyesh Parekh <divyesh.parekh@attuneinfocom.in>
2014-09-09 10:49:11 +0530

    third

--- rpmbuild.sh
+++ rpmbuild.sh
@@ -5,3 +5,9 @@ mkdir -p rpm/{RPMS,SRPMS,SPEC,SOURCE,BUILD,tmp}
 cd rpm/SOURCE
 mkdir pkgsource-1
 cd pkgsource-1
+cp -irf ../../../../source/* ./
+cd ..
+tar -czvf pkgsource-1.tar.gz pkgsource-1
+cd ..
+rpmdev-newspec SPEC/pkgsource.spec
+
